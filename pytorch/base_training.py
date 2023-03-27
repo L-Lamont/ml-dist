70c6a56dd69a026564cc0894cfbe2705a80866a8
L-Lamont <74656778+L-Lamont@users.noreply.github.com>
2023-03-27 10:54:29 +0800

    removed unused import in base_training.py

--- pytorch/base_training.py
+++ pytorch/base_training.py
@@ -10,7 +10,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-import os
 
 
 class Net(nn.Module):
